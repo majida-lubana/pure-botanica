3c78880ea2c9b461135a7f5818f4f06efc21ec37
Majida Lubana <majidarashi7994@gmail.com>
2026-01-07 01:08:11 +0530

    refactor: update config files to ES module imports

--- config/db.js
+++ config/db.js
@@ -1,16 +1,22 @@
-const mongoose = require("mongoose")
-const env = require("dotenv").config()
 
-// suppress warning
+
+import mongoose from 'mongoose';
+
+import logger from '../utils/logger.js';
+
+
+
+
 mongoose.set('strictQuery', true);
 
-const connectDB  = async()=>{
-    try{
-        await mongoose.connect(process.env.MONGODB_URI)
-        console.log("DB connected")
-    }catch(error){
-        console.log('DB connection error',error.message)
-        process.exit()
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("DB connected");
+    } catch (error) {
+        logger.error('DB connection error', error.message);
+        process.exit(1); 
     }
-}
-module.exports = connectDB
+};
+
+export default connectDB;
--- config/passport.js
+++ config/passport.js
@@ -1,58 +1,63 @@
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const User = require('../models/userSchema')
-require('dotenv').config()
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3002/google/callback'
-  },
-  async (accessToken, refreshToken, profile, done) => {
-    console.log('=== Google Strategy Callback ===');
-    console.log('Profile ID:', profile.id);
-    console.log('Profile email:', profile.emails[0].value);
-    
-    try {
-      const existingUser = await User.findOne({ googleId: profile.id });
-      if (existingUser) {
-        console.log('Existing user found:', existingUser._id);
-        return done(null, existingUser);
+
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import User from '../models/userSchema.js';
+
+const {
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  GOOGLE_CALLBACK_URL
+} = process.env;
+
+console.log("GOOGLE:", process.env.GOOGLE_CLIENT_ID ? "OK" : "MISSING");
+
+
+if (!GOOGLE_CLIENT_ID || !GOOGLE_CLIENT_SECRET) {
+  console.warn(' Google OAuth disabled: missing client ID or secret');
+} else {
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: GOOGLE_CLIENT_ID,
+        clientSecret: GOOGLE_CLIENT_SECRET,
+        callbackURL: GOOGLE_CALLBACK_URL || 'http://localhost:3002/google/callback',
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        try {
+          const email = profile.emails?.[0]?.value;
+
+          let user = await User.findOne({ googleId: profile.id });
+
+          if (!user) {
+            user = await User.create({
+              googleId: profile.id,
+              name: profile.displayName,
+              email,
+            });
+          }
+
+          return done(null, user);
+        } catch (error) {
+          return done(error, null);
+        }
       }
+    )
+  );
+}
 
-      const newUser = new User({
-        googleId: profile.id,
-        name: profile.displayName,
-        email: profile.emails[0].value,
-      });
-      await newUser.save();
-      console.log('New user created:', newUser._id);
-      done(null, newUser);
-    } catch (error) {
-      console.log('Error in Google strategy:', error);
-      done(error, null);
-    }
-  }
-));
 
 passport.serializeUser((user, done) => {
-    console.log('=== Serializing User ===');
-    console.log('User._id:', user._id);
-    done(null, user._id);
+  done(null, user._id);
 });
 
-passport.deserializeUser((id, done) => {
-    console.log('=== Deserializing User ===');
-    console.log('Looking for user ID:', id);
-    User.findById(id)
-    .then(user => {
-        console.log('Found user:', user);
-        done(null, user);
-    })
-    .catch(err => {
-        console.log('Deserialize error:', err);
-        done(err, null);
-    });
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
 });
 
-module.exports = passport;
+export default passport;
