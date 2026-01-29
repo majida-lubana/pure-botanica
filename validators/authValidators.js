eaa5bda275808c21b0d7f58da5140b2e81fc9d0c
Majida <majida@gmail.com>
2026-01-29 10:29:22 +0530

    email validation

--- validators/authValidators.js
+++ validators/authValidators.js
@@ -18,9 +18,16 @@ export const signupSchema = z.object({
     .max(50, "Name must not exceed 50 characters")
     .regex(/^[a-zA-Z\s]+$/, "Name can only contain letters and spaces"),
 
+  // email: z.string()
+  //   .email("Invalid email format")
+  //   .toLowerCase(),
   email: z.string()
-    .email("Invalid email format")
-    .toLowerCase(),
+  .regex(
+    /^[a-zA-Z0-9._%+-]{2,}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+    "Email must contain at least 2 characters before @"
+  )
+  .toLowerCase(),
+
   
   phone: z.string()
     .min(10, "Phone number must be at least 10 digits")
